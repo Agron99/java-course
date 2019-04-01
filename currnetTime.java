f68e7e2e6feddc9dfabd0cad2c85d1af65ada266
mohamadjavad <9636583@stu.yazd.ac.ir>
2019-04-02 02:46:57 +0430

    update currenttime.java
    added comment to code

--- currnetTime.java
+++ currnetTime.java
@@ -6,7 +6,7 @@ public class currnetTime {
     public static void main(String[] args) {
         while(true) {
         Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");//format e hour minute second
 
             System.out.print("\rcurrent time of your pc is:    " + sdf.format(cal.getTime()));
         }
